f7e54d7e5e407ba6b68359b1f58d6a519091408a
ns-disha-sachdeva-NTZ2280 <disha.sachdeva@netsmartz.com>
2026-08-11 16:56:50 +0530

    Update image-annotation.js

--- image-annotation.js
+++ image-annotation.js
@@ -1,1 +1,46 @@
 
+(function () {
+  const Field = Formio.Components.components.field;
+
+  class ImageAnnotationComponent extends Field {
+
+    static schema(...extend) {
+      return Field.schema({
+        type: 'imageAnnotation',
+        label: 'Image Annotation',
+        key: 'imageAnnotation',
+        input: true,
+        persistent: true,
+        imageUrl: '',
+        ...extend
+      });
+    }
+
+    static get builderInfo() {
+      return {
+        title: 'Image Annotation',
+        icon: 'picture-o',
+        group: 'basic',
+        weight: 10,
+        schema: ImageAnnotationComponent.schema()
+      };
+    }
+
+    render() {
+      return super.render(`
+        <div class="image-annotation-component">
+          <img
+            ref="image"
+            src="${this.component.imageUrl}"
+            style="max-width:100%; display:block;"
+          />
+        </div>
+      `);
+    }
+  }
+
+  Formio.Components.addComponent(
+    'imageAnnotation',
+    ImageAnnotationComponent
+  );
+})();
